f947d431fa226b1de4ef5ac8f63c8e0e19b54271
Felix <93457386+feliquisds@users.noreply.github.com>
2024-09-27 12:06:52 -0300

    feat(poo-08); extra work is done

--- poo-08/src/Client.java
+++ poo-08/src/Client.java
@@ -1,60 +1,68 @@
-import java.util.Date;
+import java.time.Period;
+import java.time.LocalDate;
 
 public class Client {
+    // attributes
     private String name;
-    private Date birth_date;
+    private LocalDate birth_date;
     private String email;
     private String phone;
     private String id;
-    private Date creation_date;
+    private LocalDate creation_date;
 
-    public Client(String name, Date birth_date, String email, String phone) {
+
+
+    // constructors
+    public Client(String name, LocalDate birth_date, String email, String phone) {
         this.name = name;
         this.birth_date = birth_date;
         this.email = email;
         this.phone = phone;
-
-        id = String.valueOf(Math.random() * 100);
-        creation_date = new Date();
+        createAccountID();
     }
 
-
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
+    public Client(String name, LocalDate birth_date, String email) {
         this.name = name;
-    }
-
-
-
-    public Date getBirth_date() {
-        return birth_date;
-    }
-    public void setBirth_date(Date birth_date) {
         this.birth_date = birth_date;
-    }
-
-
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
         this.email = email;
+        createAccountID();
+    }
+
+    private void createAccountID() {
+        id = String.valueOf(Math.round(Math.random() * 100));
+        creation_date = LocalDate.now();
     }
 
 
 
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+    // getters and setters
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
 
-    
+    public LocalDate getBirth_date() {return birth_date;}
+    public void setBirth_date(LocalDate birth_date) {this.birth_date = birth_date;}
 
-    
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getPhone() {return phone;}
+    public void setPhone(String phone) {this.phone = phone;}
+
+
+
+    // getters-only
+    public String getId() {return id;}
+
+    public LocalDate getCreation_date() {return creation_date;}
+
+
+
+    // methods
+    public int getAge() {return Period.between(birth_date, LocalDate.now()).getYears();}
+
+    public int getYearsSinceAccountCreation() {return Period.between(creation_date, LocalDate.now()).getYears();}
+
+    public int getAgeOnAccountCreation() {return Period.between(birth_date, creation_date).getYears();}
+
+    public boolean isOver18() {return getAge() >= 18;}
 }
