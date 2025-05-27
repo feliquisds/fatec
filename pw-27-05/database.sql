347828a58a8b5c4b842dce4d809989c1daa78d6a
feliquisds <mail@feliquisds.gay>
2025-05-27 12:51:26 -0300

    composer class

---
+++ pw-27-05/database.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA fatec;
+
+USE fatec;
+
+CREATE TABLE aluno (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(60) NOT NULL,
+	genero VARCHAR(10)
+);
---
+++ pw-27-05/src/models/entity/aluno.php
@@ -0,0 +1,34 @@
+<?php
+    namespace src\models\entity;
+
+    class aluno {
+        private $id;
+        private $nome;
+        private $genero;
+
+        public function __construct($nome, $genero) {
+            $this -> nome = $nome;
+            $this -> genero = $genero;
+        }
+
+        public function getId() {
+            return $this -> id;
+        }
+
+        public function getNome() {
+            return $this -> nome;
+        }
+
+        public function getGenero() {
+            return $this -> genero;
+        }
+
+        public function setNome($nome) {
+            $this -> nome = $nome;
+        }
+
+        public function setGenero($genero) {
+            $this -> genero = $genero;
+        }
+    }
+?>
